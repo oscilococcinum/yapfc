57dbfce0279e2bce38933ee1c376783e37d7b5c4
oscilococcinum <gawlikbar@gmail.com>
2026-01-19 14:46:52 +0100

    remeoves loading stl as computional mesh

--- yapfc/mesh.py
+++ yapfc/mesh.py
@@ -4,19 +4,19 @@ import meshio
 
 class Mesh():
     def __init__(self, fpath: str) -> None:
-        self._mesh: vtk.vtkUnstructuredGrid | vtk.vtkPolyData = self._loadMesh(fpath)
+        self._mesh: vtk.vtkUnstructuredGrid = self._loadMesh(fpath)
         self._actor: vtk.vtkActor = self._MapGridToActor(self._mesh)
         self._setColors()
 
     #Getters
-    def getMesh(self) -> vtk.vtkUnstructuredGrid | vtk.vtkPolyData:
+    def getMesh(self) -> vtk.vtkUnstructuredGrid:
         return self._mesh
     
     def getActor(self) -> vtk.vtkActor:
         return self._actor
     
     #Internal
-    def _loadMesh(self, fpath: str) -> vtk.vtkUnstructuredGrid | vtk.vtkPolyData:
+    def _loadMesh(self, fpath: str) -> vtk.vtkUnstructuredGrid:
         if fpath.endswith(".msh") or fpath.endswith(".mesh"):
             mesh: meshio.Mesh = meshio.read(fpath)
 
@@ -37,17 +37,11 @@ class Mesh():
             grid.SetPoints(vtk_points)
             grid.SetCells(vtk.VTK_TETRA, vtk_cells)
             return grid
-        elif fpath.endswith(".stl"):
-            reader = vtk.vtkSTLReader()
-            reader.SetFileName(fpath)  # Replace with your VTK file path
-            reader.Update()
-            grid = reader.GetOutput()
-            return grid
         else:
             print(f'This format is not yet implemented')
             return vtk.vtkSTLReader().GetOutput()
 
-    def _MapGridToActor(self, grid: vtk.vtkUnstructuredGrid | vtk.vtkPolyData | None):
+    def _MapGridToActor(self, grid: vtk.vtkUnstructuredGrid | None) -> vtk.vtkActor:
         mapper = vtk.vtkDataSetMapper()
         mapper.SetInputData(grid)
         actor = vtk.vtkActor()
